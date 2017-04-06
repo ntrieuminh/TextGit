dd37cb0e2994a949015069102fb09fb4be6aa89e
ntrieuminh <ntrieuminh@gmail.com>
2017-04-07 06:55:38 +0700

    testGit demo
    Change-Id: I2f352a9a3c546d1bb16f62654a2eedb415aa3d40
    Signed-off-by: ntrieuminh <ntrieuminh@gmail.com>

---
+++ textGit/src/main/java/TextGit/textGit/App.java
@@ -0,0 +1,13 @@
+package TextGit.textGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ textGit/src/test/java/TextGit/textGit/AppTest.java
@@ -0,0 +1,13 @@
+package TextGit.textGit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AppTest {
+public void f() {
+	  
+	  WebDriver driver = new FirefoxDriver();
+		 driver.get("https://www.google.com");
+		 driver.quit();
+}
+}
